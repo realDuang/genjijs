aa4b131afdbc4baeba7e0e845084f7f9d2858175
synccheng <synccheng@tencent.com>
2019-10-12 11:19:02 +0800

    :tada: 初始化项目,建立genji框架以及测试demo

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import createStore from "./createStore";
+import combineReducer from "./combineReducer";
+
+class Genji {
+  constructor() {
+    this._units = [];
+    this._reducers = [];
+    this._states = {};
+  }
+
+  unit(unit) {
+    this._units.push(unit);
+  }
+
+  start() {
+    // 注册所有的state与reducers
+    for (let i = 0; i < this._units.length; i) {
+      this._states[this._units[i].namespace] = {
+        ...this._units[i].state,
+      };
+      Reflect.ownKeys(this._units[i].reducers).forEach(key => {
+        this._reducers.push(this._units[i].reducers[key]);
+      });
+    }
+    const rootReducer = combineReducer(this._reducers);
+    this._store = createStore(rootReducer, this._states);
+  }
+}
+
+export default Genji;
---
+++ test.js
@@ -0,0 +1,56 @@
+const addModel = {
+  namespace: "add",
+  state: {
+    num: 0,
+  },
+  reducers: {
+    add(state, { payload }) {
+      console.log(`answer ${state.num + payload.addNum}`);
+      return {
+        num: state.num + payload.addNum,
+      };
+    },
+    getNum(state, action) {
+      console.log(`getNum ${state.num}`);
+      return state.num;
+    },
+  },
+};
+
+const userModel = {
+  namespace: "user",
+  state: {
+    name: "",
+  },
+  reducers: {
+    modify(state, { payload }) {
+      const { name } = payload;
+      console.log(`change name ${name}`);
+      return {
+        name,
+      };
+    },
+  },
+};
+
+import Genji from "./src/index";
+const app = new Genji();
+app.unit(addModel);
+app.unit(userModel);
+
+app.start();
+app._store.dispatch({
+  type: "addModel/add",
+  payload: {
+    addNum: 1,
+  },
+});
+
+app._store.dispatch({
+  type: "user/modify",
+  payload: {
+    name: "synccheng",
+  },
+});
+
+console.log(app);
